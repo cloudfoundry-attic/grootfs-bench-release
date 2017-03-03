f0254d5401277099691e9bdb8d174f8d854fba94
Roberto Jimenez Sanchez <jszroberto@gmail.com>
2017-03-03 11:42:05 +0000

    Use bosh2 to create grootfs-bench-release
    [#139670309]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/create-grootfs-bench-release.sh
+++ ci/scripts/create-grootfs-bench-release.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 VERSION=$(cat grootfs-bench-release-version/number)
+BUILD_FOLDER=$PWD
 
 pushd grootfs-bench-release
-  bosh create release --force --version $VERSION --with-tarball --name grootfs-bench
+  bosh2 create-release --force --version $VERSION --tarball $BUILD_FOLDER/bosh-release/grootfs-bench-$VERSION.tgz --name grootfs-bench
 popd
-
-mv grootfs-bench-release/dev_releases/grootfs-bench/grootfs-bench-*.tgz bosh-release/
