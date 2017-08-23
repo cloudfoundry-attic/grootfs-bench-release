3c1ecdea577aae58d0ce11c7d0de20916e661b11
Gareth Clay <gclay@pivotal.io>
2017-08-23 13:40:39 +0100

    Remove bosh2 references from grootfs-bench-release
    The bosh 2 go cli is now named 'bosh'
    
    [#150474707]

--- ci/scripts/create-grootfs-bench-release.sh
+++ ci/scripts/create-grootfs-bench-release.sh
@@ -4,5 +4,5 @@ VERSION=$(cat grootfs-bench-release-version/number)
 BUILD_FOLDER=$PWD
 
 pushd grootfs-bench-release
-  bosh2 create-release --force --version $VERSION --tarball $BUILD_FOLDER/bosh-release/grootfs-bench-$VERSION.tgz --name grootfs-bench
+  bosh create-release --force --version $VERSION --tarball $BUILD_FOLDER/bosh-release/grootfs-bench-$VERSION.tgz --name grootfs-bench
 popd
--- ci/scripts/performance-tests.sh
+++ ci/scripts/performance-tests.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "$BOSH_CERTIFICATES" > certificates.yml
-bosh2 int --path "/certs/ca_cert" certificates.yml > ca_cert.crt
+bosh int --path "/certs/ca_cert" certificates.yml > ca_cert.crt
 
 echo "Running performance tests..."
-bosh2 -e $BOSH_TARGET --ca-cert ca_cert.crt alias-env bosh-director
-bosh2 -e bosh-director --client $BOSH_CLIENT --client-secret $BOSH_CLIENT_SECRET login
-bosh2 -e bosh-director -d $BOSH_DEPLOYMENT run-errand $ERRAND_NAME
+bosh -e $BOSH_TARGET --ca-cert ca_cert.crt alias-env bosh-director
+bosh -e bosh-director --client $BOSH_CLIENT --client-secret $BOSH_CLIENT_SECRET login
+bosh -e bosh-director -d $BOSH_DEPLOYMENT run-errand $ERRAND_NAME
